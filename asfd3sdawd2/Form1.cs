75c5257d4c66c8a2a4f2d965d233f2362b62c583
EwanRedgrift <ewanredg500@ed.amdsb.ca>
2024-04-18 13:55:21 -0400

    Add project files.

---
+++ asfd3sdawd2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace asfd3sdawd2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen yellowPen = new Pen(Color.Yellow, 10);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+
+            Font drawFont = new Font("OfficinaSansStd-Bold", 50, FontStyle.Bold);
+
+            g.Clear(Color.White);
+
+            g.DrawString("amazon", drawFont, blackBrush, 0, 0);
+            g.DrawArc(yellowPen, 25, 40, 150, 75, 0, 180);
+            g.FillPie(yellowBrush, 160, 60, 50, 25, 60, 120);
+        }
+    }
+}
